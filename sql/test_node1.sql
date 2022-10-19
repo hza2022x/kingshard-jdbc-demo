b7f4f0a5f7fbab8c4e918417576be4e5f20c57f4
wangxiaoming <wangxiaoming@joyy.com>
2022-10-19 15:26:16 +0800

    init

---
+++ sql/test_node1.sql
@@ -0,0 +1,29 @@
+create database kingshard;
+use kingshard;
+create table test_shard_hash_0000
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+create table test_shard_hash_0001
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+create table test_shard_hash_0002
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+create table test_shard_hash_0003
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+
+
+
---
+++ sql/test_node2.sql
@@ -0,0 +1,25 @@
+create database kingshard;
+use kingshard;
+create table test_shard_hash_0004
+(
+    id   int primary key,
+    name varchar(20)
+);
+create table test_shard_hash_0005
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+create table test_shard_hash_0006
+(
+    id   int primary key,
+    name varchar(20)
+);
+
+create table test_shard_hash_0007
+(
+    id   int primary key,
+    name varchar(20)
+);
+
